05894ecb4ba32142afb73c8a1c86138c47f22736
Sophia <99284300+sophiav17@users.noreply.github.com>
2022-11-15 17:54:31 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload() {
+
+}
+function preload() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+function draw() {
+
+}
+function take_snapshot() {
+    save('myFilterImage.png');
+}
